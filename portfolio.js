2de287949f663e02806e489fdd5bce867986c900
DiscoSloth <edobs47@gmail.com>
2022-03-12 22:22:45 -0500

    update for HW
    HW 19 done!

--- portfolio.js
+++ portfolio.js
@@ -1,8 +1,13 @@
 window.onload = function() {
 const btn = document.getElementById("btn");  
 
-btn.addEventListener('Click',function() {
+btn.addEventListener('mousedown',function() {
     this.style.backgroundColor = '#755A30';
+    this.style.color = 'white';
+});
+
+btn.addEventListener('mouseup',function() {
+    this.style.backgroundColor = '#AD956E';
     this.style.color = 'white';
 });
 
